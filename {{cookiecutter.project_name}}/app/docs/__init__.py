de8126d677dd4b05ba34d0b024d2aedc6992b117
Hermes Trismegistus <91907013+herumes@users.noreply.github.com>
2024-04-04 01:31:40 -0700

    Update __init__.py

--- {{cookiecutter.project_name}}/app/docs/__init__.py
+++ {{cookiecutter.project_name}}/app/docs/__init__.py
@@ -4,8 +4,8 @@ This module contains OpenAPI Documentation definition for the API.
 It exposes a docs object that can be used to decorate request handlers with additional
 information, used to generate OpenAPI documentation.
 """
-from blacksheep import Application
-from blacksheep.server.openapi.v3 import OpenAPIHandler
+from shuttleasgi import Application
+from shuttleasgi.server.openapi.v3 import OpenAPIHandler
 from openapidocs.v3 import Info
 
 from app.settings import Settings
